0d573157dac8fc628be18ee5b192ba3857614261
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-07-08 05:57:54 +0900

    [p]7/8

---
+++ wcp/Abstract01.java
@@ -0,0 +1,11 @@
+package extend;
+
+import extend.animal.Dog;
+
+public class Abstract01 {
+  public static void main(String[] args){
+    Dog kogi = new Dog();
+    kogi.eat();
+    kogi.cry();
+  }
+}
