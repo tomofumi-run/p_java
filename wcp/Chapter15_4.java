1516ded709a18fde1a1d8e5e71acae6131358f7a
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-07-08 11:13:41 +0900

    [p]7/8

---
+++ wcp/Chapter15_4.java
@@ -0,0 +1,17 @@
+public class Chapter15_4 {
+  public static void main(String[] args) {
+      for (int i = 1; i <= 2; i++) {
+          ThreadSample mt = new ThreadSample(i);
+          thread.start();
+      }
+
+      for (int i = 1; i <= 3; i++) {
+          try {
+              Thread.sleep(1000);
+              System.out.println("Chapter15_4のスレッド : " + i + "回目");
+          } catch (InterruptedException e) {
+              e.printStackTrace();
+          }
+      }
+  }
+}
