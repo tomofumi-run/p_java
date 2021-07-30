09aab9e0a166467430069c4ecf9a935e1f8109dc
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-07-30 09:10:04 +0900

    [p]7/30

---
+++ session_8/cleaning/KyotoCleaningShop.java
@@ -0,0 +1,17 @@
+public class KyotoCleaningShop implements CleaningService {
+  private String ownerName;
+  private String address;
+  private String phone;
+  // シャツを洗う
+  public Shirt washShirt(Shirt s){
+
+  }
+// タオルを洗う
+  public Towl washTowl(Towl t){
+
+  }
+// コートを洗う
+  public Coat washCoat(Coat c){
+
+  }
+}
