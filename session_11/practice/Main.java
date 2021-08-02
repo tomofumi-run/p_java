16a248a43b7a8bc0df3e8a37b85e4c2b13c7514f
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-08-02 09:09:36 +0900

    [p]8/2

---
+++ session_11/practice/Main.java
@@ -0,0 +1,25 @@
+import java.io.*;
+public class Main {
+  public static void main(String[] args){
+    FileWriter fw = new FileWriter("da");
+    // String s = "三";
+    // try {
+    //   int n = Integer.parseInt(s);
+    // } catch (NumberFormatException e) {
+    //   System.out.println("文字列は認識されません。");
+    // }
+
+
+    // try {
+    //   System.out.println(n);
+    // } catch (NullPointerException e) {
+    //   System.out.println("-- スタックトレース(ここから) --");
+    //   e.printStackTrace();
+    //   System.out.println("-- スタックトレース(ここまで) --");
+    // } catch(NumberFormatException e){
+    //   System.out.println("-- スタックトレース(ここから) --");
+    //   e.printStackTrace();
+    //   System.out.println("-- スタックトレース(ここまで) --");
+    // }
+  }
+}
