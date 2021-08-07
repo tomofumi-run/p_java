15c3351768bbddf38fc8c4e0a08b6eec048d6552
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-08-07 13:55:54 +0900

    [p]

--- matome/Main.java
+++ matome/Main.java
@@ -1,21 +1,10 @@
 public class Main {
   public static void main(String[] args){
-    int tmp = 30;
-    System.out.println("現在の室温は" + tmp + "です。");
-    System.out.println("エアコンをつけますか？ Yes or No");
-    String input = new java.util.scanner(system.in).nextline();
-    if(input == "Yes"){
-      System.out.println("エアコンを起動");
-      while(tmp < 25){
-        tmp--;
-        System.out.println("室温が"+ tmp +"に下がりました。")
+    System.out.println("0から20まで2の倍数のみ答えます。");
+    for(int i = 0; i < 21; i++){
+      if(i % 2 == 0){
+        System.out.println(i);
       }
-      System.out.println("最適な温度に下がりました。エアコンを停止します。");
-    } else if(input == "No") {
-      System.out.println("まぁ、無理だけはするなよ。");
-    } else {
-      System.out.println("YesかNoかで指示してください。それ以外は受け付けられません。");
     }
-
   }
 }
