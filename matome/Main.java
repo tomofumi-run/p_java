813196236352b2e40798c62c9cf293bebd7e97cd
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-08-07 13:23:07 +0900

    [p]

--- matome/Main.java
+++ matome/Main.java
@@ -1,23 +1,21 @@
 public class Main {
   public static void main(String[] args){
-    System.out.println("好きなポケモンを選びなさい。");
-    int pokemon = 6;
-
-    switch (pokemon) {
-      case 1:
-        System.out.println("ピカチュウに決めた！");
-        break;
-      case 2:
-        System.out.println("ヒトカゲに決めた！");
-        break;
-      case 3:
-        System.out.println("ゼニガメに決めた！");
-        break;
-      case 4:
-        System.out.println("フシギダネに決めた！");
-        break;
-      default:
-        System.out.println("オーキドに決めた！");
+    int tmp = 30;
+    System.out.println("現在の室温は" + tmp + "です。");
+    System.out.println("エアコンをつけますか？ Yes or No");
+    String input = new java.util.scanner(system.in).nextline();
+    if(input == "Yes"){
+      System.out.println("エアコンを起動");
+      while(tmp < 25){
+        tmp--;
+        System.out.println("室温が"+ tmp +"に下がりました。")
+      }
+      System.out.println("最適な温度に下がりました。エアコンを停止します。");
+    } else if(input == "No") {
+      System.out.println("まぁ、無理だけはするなよ。");
+    } else {
+      System.out.println("YesかNoかで指示してください。それ以外は受け付けられません。");
     }
+
   }
 }
