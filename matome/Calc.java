cb601c02098332ffac072dd5a8d62941e185dbe3
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-08-11 07:42:17 +0900

    [p]

---
+++ matome/Calc.java
@@ -0,0 +1,10 @@
+public class Calc{
+  public static int tasukake(int a, int b){
+    int tasu = a + b;
+    return (tasu * 2);
+  }
+  public static int hikuwaru(int a, int b){
+    int hiku = a - b;
+    return (hiku / 2);
+  }
+}
--- matome/Main.java
+++ matome/Main.java
@@ -1,25 +1,6 @@
 public class Main {
   public static void main(String[] args){
-    String name = "ほげ丸ほげ郎";
-    int age = 30;
-    hello(name);
-    ask(age);
-    kind(lie(age));
-  }
-
-  public static void hello(String name){
-    System.out.println(name + "さん、こんにちは。");
-  }
-
-  public static void ask(int age){
-    System.out.println("ご年齢はおいくつですか？");
-    System.out.println(age + "歳なんですか！");
-  }
-
-  public static int lie(int age){
-    return age - 10;
-  }
-  public static void kind(int lie){
-    System.out.println(lie + "歳かと思いました！w");
+    System.out.println(Calc.tasukake(3,2));
+    System.out.println(Calc.hikuwaru(4,2));
   }
 }
