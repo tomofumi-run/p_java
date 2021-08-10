9fa72ac290b0cb89be35f2ecb238d23c1a2ff635
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-08-10 17:22:17 +0900

    [p]

--- matome/Main.java
+++ matome/Main.java
@@ -1,21 +1,25 @@
 public class Main {
   public static void main(String[] args){
-    getAdd("はろー",2);
-    getSub(2,2);
-    getMul(2,2);
-    getDiv(2,2);
+    String name = "ほげ丸ほげ郎";
+    int age = 30;
+    hello(name);
+    ask(age);
+    kind(lie(age));
   }
 
-  public static void getAdd(int x, int y){
-    System.out.println(x + y);
+  public static void hello(String name){
+    System.out.println(name + "さん、こんにちは。");
   }
-  public static void getSub(int x, int y){
-    System.out.println(x - y);
+
+  public static void ask(int age){
+    System.out.println("ご年齢はおいくつですか？");
+    System.out.println(age + "歳なんですか！");
   }
-  public static void getMul(int x, int y){
-    System.out.println(x * y);
+
+  public static int lie(int age){
+    return age - 10;
   }
-  public static void getDiv(int x, int y){
-    System.out.println(x / y);
+  public static void kind(int lie){
+    System.out.println(lie + "歳かと思いました！w");
   }
 }
