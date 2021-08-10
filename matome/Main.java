35d291327d46fb8a0f7b87c081600ea7e9e1d82c
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-08-10 17:13:32 +0900

    [p]

--- matome/Main.java
+++ matome/Main.java
@@ -1,16 +1,21 @@
 public class Main {
   public static void main(String[] args){
-    int a = 4;
-    int b = 2;
-    int c = 6;
-    int d = 3;
-    System.out.println(a + b);
-    System.out.println(a - b);
-    System.out.println(a / b);
-    System.out.println(a * b);
-    System.out.println(c + d);
-    System.out.println(c - d);
-    System.out.println(c / d);
-    System.out.println(c * d);
+    getAdd("はろー",2);
+    getSub(2,2);
+    getMul(2,2);
+    getDiv(2,2);
+  }
+
+  public static void getAdd(int x, int y){
+    System.out.println(x + y);
+  }
+  public static void getSub(int x, int y){
+    System.out.println(x - y);
+  }
+  public static void getMul(int x, int y){
+    System.out.println(x * y);
+  }
+  public static void getDiv(int x, int y){
+    System.out.println(x / y);
   }
 }
