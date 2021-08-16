80d6747b7910df24eaf8a49a70be2b3a0a5acc29
token <nnsu_1014@yahoo.co.jp>
2021-08-16 11:38:30 +0900

    [p]

--- matome/main/Human.java
+++ matome/main/Human.java
@@ -1,4 +1,4 @@
-public class Human {
+public abstract class Human {
   private String name;
   private int money;
 
--- matome/main/Main.java
+++ matome/main/Main.java
@@ -1,16 +1,5 @@
 public class Main{
   public static void main(String[] args){
-    Teacher t = new Teacher("ほげ先生", 200000);
     Engineer e = new Engineer("ほげSE", 150000);
-    Unemployed u = new Unemployed("ほげニート", 0);
-    t.call();
-    t.teach();
-
-    e.call();
-    e.assemble();
-    
-    u.call();
-    u.troublesome();
-
   }
 }
